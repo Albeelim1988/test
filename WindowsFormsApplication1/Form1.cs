bb9075f675320637c5834fb82cda16efca6aa827
User1 <User1@PC01>
2016-04-25 16:03:57 +0800

    Add project files.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x = 4, y = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int sum;
+            sum=x + y;
+            Console.Write(sum);
+           
+
+        }
+    }
+}
