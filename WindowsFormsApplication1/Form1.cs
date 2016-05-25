d3f17afabe38d00106d7f35601ce560f604ecc06
User1 <User1@PC01>
2016-05-25 09:41:09 +0800

    added comment

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 //using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,15 +21,37 @@ namespace WindowsFormsApplication1
         int x = 4, y = 1;
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
             int sum;
-            sum=x + y;
+            sum = x + y;
             Console.Write(sum);
             x = 3;
             y = x * 3;
             sum = x + y;
             sum = x - y;
-            //FileStraem fileO = File.OpenRead("text.txt"); demo 
+            //FileStraam fileO = File.OpenRead("text.txt"); //demo 
+
+            /*test test1 = new test(1);
+            test test2 = new test(2);
+            test test3 = new test(3);*/
+            int test1 = 1; //comment
+            /*switch (test1)
+            {
+                case (test1 <9):
+                    Console.WriteLine("Same as Case 2");
+                    break;
+                case test3:
+                    Console.WriteLine("Same as Case 3");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }*/
         }
+    }
+    class test
+    {
+        public int value { get; set; }
+        public test(int x) { value = x; }
     }
 }
